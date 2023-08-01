073bfe5e6a5569fee401916a732cc753366f3430
joanalawer <lawerjoana477@gmail.com>
2023-08-01 17:43:12 +0000

    Update javascript

--- responsive-navbar/app.js
+++ responsive-navbar/app.js
@@ -1,1 +1,17 @@
-const hamburger = document.querySelector
+const hamburger = document.querySelector('.hamburger');
+const menuLinks = document.querySelector('menu-links');
+const menuLinkItems = Array.from(menuLinks.querySelectorAll('a'));
+
+hamburger.addEventListener('click', () => {
+    menuLinks.classList.toggle('active');
+});
+
+menuLinkItems.forEach((link) => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        menuLinkItems.forEach((item) => {
+            item.classList.remove('active');
+        });
+        link.classList.add('active');
+    });
+});
